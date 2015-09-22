dce1e3cd1ce48ebac0f03022244cd0682a49a39a
Elijah <Elijahtylerbrown@outlook.com>
2015-09-22 19:12:18 -0400

    first commit

---
+++ app/controllers/atm_controller.rb
@@ -0,0 +1,30 @@
+class AtmController < ApplicationController
+  def home
+  	atmaction = params[:atmaction]
+  	@balance = RailsATM::Application.config.balance
+  	@message = ""
+
+  	case atmaction
+  	when 'withdraw'
+  		@amount = params[:w_amount].to_f
+  		if @balance >= @amount
+  			@balance = @balance - @amount 
+  			@message = "Withdrawal Complete. Thank You for your business."
+  		else
+  			@message = " You already knew you didn't have enough. You was hoping for a miracle!"
+  	end
+  	when 'deposit'
+  		@amount = params[:d_amount].to_f
+  		@balance = @balance + @amount
+  		@message = "Deposit complete. Thank you for your business."
+
+  	when 'balance'
+  		@message = "Your balance is: #{@balance}"
+  	
+  	end
+
+  	RailsATM::Application.config.message = @message
+  	RailsATM::Application.config.balance = @balance
+
+  end
+end
